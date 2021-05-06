44294a845e84fa87fd8c0686ea0170b2e78d0d15
Алексей Гришин <83308525+abslic@users.noreply.github.com>
2021-05-07 02:43:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Метод_подсчета_количества_цифр
+{
+    class Program
+    {
+        public static int Main()
+        {
+            Console.Write("Введите число:");
+            int num = int.Parse(Console.ReadLine());
+        
+            int i= 0;
+            while (num != 0)
+            {
+                i++;
+                num /= 10;
+                
+            }
+            
+            Console.WriteLine("Количество цифр введенного числа:{0}",i);
+            Console.ReadKey();
+            return i;
+
+        }
+    }
+}
